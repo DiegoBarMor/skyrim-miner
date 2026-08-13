a8185db4ff71c2324bd9211c92f11f1560d6aba8
DBM <diegobarmor42@gmail.com>
2026-08-13 11:22:01 +0200

    added script to extract relevant metadata for statics entries

---
+++ src/extract_metadata_statics.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import pandas as pd
+
+# ------------------------------------------------------------------------------
+def main():
+    df_fcd = pd.read_csv(PATH_TSV_FCD, sep = '\t')
+    df_statics = df_fcd[df_fcd["Form Type"] == "STAT"]
+    df_statics = df_statics[["Editor ID", "Numeric ID", "Health"]] # path to texture is incorrectly stored in the "Health" column
+    df_statics = df_statics.rename(columns = {
+        "Editor ID": "name",
+        "Numeric ID": "base_form",
+        "Health": "path_texture",
+    })
+    df_statics["base_form"] = df_statics["base_form"].str.strip("()")
+    df_statics = df_statics.sort_values(by = "base_form")
+    df_statics.to_csv(PATH_CSV_OUT, index = False)
+
+
+
+################################################################################
+if __name__ == "__main__":
+    FOLDER_DATA = Path("data")
+    PATH_TSV_FCD = FOLDER_DATA / "form_component_data.tsv"
+    PATH_CSV_OUT = FOLDER_DATA / "statics.meta.csv"
+
+    main()
+
+
+################################################################################
+# python3 src/extract_metadata_statics.py
